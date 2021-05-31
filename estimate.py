2905ace480f9f72adcda29625e51093b23fd8f69
Sarath Wesley <wesleysarath1@gmail.com>
2021-05-31 19:16:30 +0530

    adds wallis and monte_carlo

--- estimate.py
+++ estimate.py
@@ -21,7 +21,7 @@ def monte_carlo(n):
     y = random.random()
     if sqrt(x**2 + y**2) < 1:
       pts_in_circle += 1
-    else
+    else:
       pts_in_square += 1
     ct += 1
   return 4*(pts_in_circle/pts_in_square)
